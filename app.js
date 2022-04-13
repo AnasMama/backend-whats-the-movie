6b453d50b2081deca313c0165a2853b98e529b4b
Anas <anas.mama@hotmail.fr>
2022-04-13 09:56:00 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const { setupRoutes } = require('./routes');
+const express = require("express");
+require("dotenv").config();
+// const cors = require("cors");
+
+
+const app = express();
+
+
+// app.use(cors({ credentials: true}));
+app.use(express.json());
+
+// routes
+setupRoutes(app);
+
+// Please keep this module.exports app, we need it for the tests !
+module.exports = app;
